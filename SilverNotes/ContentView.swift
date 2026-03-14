c5df4e7c9231a519f358bc51a5965972ceb6f791
Marc Jurriens <marc@jurriens.nu>
2026-03-14 10:02:10 +0100

    Initial Commit

---
+++ SilverNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SilverNotes
+//
+//  Created by Marc Jurriens on 14/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SilverNotes/SilverNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SilverNotesApp.swift
+//  SilverNotes
+//
+//  Created by Marc Jurriens on 14/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SilverNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
